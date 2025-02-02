1dba82662a124f2f60812152ff5d2778902ee2dc
simone <simonegauli@gmail.com>
2025-02-02 01:10:24 +0100

    ok

--- packages/core/src/node/download.ts
+++ packages/core/src/node/download.ts
@@ -4,8 +4,6 @@ import { getUserAgent } from './user-agent.js';
 import path from 'node:path';
 import mime from 'mime-types';
 import { pipeline } from 'node:stream/promises';
-import { errorToString } from '../error.js';
-import { isProduction } from '../config.js';
 
 export interface DownloadOptions {
   directory?: string;
@@ -17,50 +15,14 @@ const defaultHeaders = {
 };
 
 /** Download a file from a given url. */
-export const download = (url: string, { headers = defaultHeaders, directory = path.join(os.homedir(), 'Downloads') }: DownloadOptions = {}) => {
-  return new Promise<string>((resolve, reject) => {
-    const handle = async (urlStr: string) => {
-      try {
-        const res = await fetch(urlStr, { headers });
-        if (!res.ok) reject(new Error(`${res.status.toString()}: ${res.statusText}`));
-        switch (res.status) {
-          case 301:
-          case 302: {
-            const location = res.headers.get('location');
-            if (!location) {
-              reject(new Error(`Request at ${url} has invalid redirect URL!`));
-              return;
-            }
-            if (!isProduction) {
-              // eslint-disable-next-line no-console
-              console.log(`Request at ${url} was redirected to ${location}...`);
-            }
-            void handle(location);
-            break;
-          }
-          default: {
-            if (res.ok) {
-              if (!res.body) {
-                reject(new Error(`Request at ${url} has no content to download.`));
-                return;
-              }
-              const filename = getFilename(urlStr, res.headers);
-              const output = path.join(directory, filename);
-              const fileStream = fs.createWriteStream(output);
-              await pipeline(res.body, fileStream);
-              resolve(output);
-            } else {
-              reject(new Error(`Download error for this url ${urlStr}: ${res.status.toString()} ${res.statusText.toString()}`));
-            }
-          }
-        }
-      } catch (err) {
-        reject(err instanceof Error ? err : new Error(errorToString(err)));
-      }
-    };
-
-    void handle(url);
-  });
+export const download = async (url: string, { headers = defaultHeaders, directory = path.join(os.homedir(), 'Downloads') }: DownloadOptions = {}) => {
+  const res = await fetch(url, { headers });
+  if (!res.ok || !res.body) throw new Error(`${res.status.toString()}: ${res.statusText}`);
+  const filename = getFilename(url, res.headers);
+  const output = path.join(directory, filename);
+  const fileStream = fs.createWriteStream(output);
+  await pipeline(res.body, fileStream);
+  return output;
 };
 
 const getFilename = (url: string, headers: Headers) => {
