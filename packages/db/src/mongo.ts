3a70f5ab514625bbb3005da198ad492e07abb925
simone <simonegauli@gmail.com>
2025-02-04 17:48:10 +0100

    make options optional

--- packages/db/src/mongo.ts
+++ packages/db/src/mongo.ts
@@ -1,16 +1,16 @@
-import { CollectionOptions, CreateIndexesOptions, type DbOptions, IndexSpecification, MongoClient, type MongoClientOptions } from 'mongodb';
+import { type CollectionOptions, type CreateIndexesOptions, type DbOptions, IndexSpecification, MongoClient, type MongoClientOptions } from 'mongodb';
 
-export const createGoatClient = (url: string, options: MongoClientOptions) => {
+export const createGoatClient = (url: string, options?: MongoClientOptions) => {
   const client = new MongoClient(url, options);
 
-  const createDb = (dbName: string, options: DbOptions) => {
+  const createDb = (dbName: string, options?: DbOptions) => {
     const db = client.db(dbName, options);
 
-    const createCollection = (name: string, options: CollectionOptions) => {
+    const createCollection = (name: string, options?: CollectionOptions) => {
       const collection = db.collection(name, options);
 
       return {
-        createIndex: (indexSpec: IndexSpecification, options: CreateIndexesOptions) => collection.createIndex(indexSpec, options),
+        createIndex: (indexSpec: IndexSpecification, options?: CreateIndexesOptions) => collection.createIndex(indexSpec, options),
       };
     };
 
