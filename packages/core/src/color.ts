6550de55f1f970a5864b5e509aa3da5e8a13d4d8
simone <simonegauli@gmail.com>
2025-02-09 19:34:15 +0100

    im

--- packages/core/src/color.ts
+++ packages/core/src/color.ts
@@ -1,5 +1,5 @@
 /* eslint-disable sonarjs/pseudo-random */
-interface HSLInput {
+export interface HSLInput {
   h?: number;
   s?: number;
   l?: number;
@@ -20,4 +20,37 @@ export const generateHSLColor = ({ h, s, l }: HSLInput = {}) => {
   const luminance = l ?? Math.floor(Math.random() * 101);
   const hsl = `hsl(${hue.toString()},${saturation.toString()}%,${luminance.toString()}%)` as const;
   return { hsl, h: hue, s: saturation, l: luminance };
+};
+
+export type RgbPixel = [number, number, number];
+
+export const rgbToHsl = (...[r, g, b]: RgbPixel) => {
+  let d,
+    h = 0,
+    s = 0;
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  const max = Math.max(r, g, b);
+  const min = Math.min(r, g, b);
+  const l = (max + min) / 2;
+  if (max !== min) {
+    d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+    switch (max) {
+      case r: {
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      }
+      case g: {
+        h = (b - r) / d + 2;
+        break;
+      }
+      case b: {
+        h = (r - g) / d + 4;
+      }
+    }
+    h /= 6;
+  }
+  return [h * 360, `${(s * 100).toString()}%`, `${(l * 100).toString()}%`];
 };
