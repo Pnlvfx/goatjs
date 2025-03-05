1a04cd264e4e09466914abae12c64df3a0cc0d45
simone <simonegauli@gmail.com>
2025-03-05 14:43:12 +0100

    o

--- packages/link-preview/src/link-preview.ts
+++ packages/link-preview/src/link-preview.ts
@@ -1,11 +1,11 @@
-import { parser } from 'html-metadata-parser';
+import metadata from 'html-metadata-parser';
 
 const urlRegex = /[\w#%()+./:=?@\\~]{2,256}\.[a-z]{2,6}\b([\w#%&+./:=?@~-]*)/gi;
 
 export const getLinkPreview = async (url: string) => {
   if (!url || !urlRegex.test(url)) throw new Error('Invalid URL');
   const { hostname } = new URL(url);
-  const { images, og, meta } = await parser(url);
+  const { images, og, meta } = await metadata.parser(url);
   const image = og.image ?? images?.at(0);
   const description = og.description ?? meta.description;
   const title = og.title ?? meta.title;
