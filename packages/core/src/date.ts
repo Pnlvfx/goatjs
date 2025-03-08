e0dc659e897a5d9ce51a50053e093161b244a5fd
simone <simonegauli@gmail.com>
2025-03-08 16:55:08 +0100

    update documentation

--- packages/core/src/date.ts
+++ packages/core/src/date.ts
@@ -30,11 +30,19 @@ export const createDate = (date: AcceptedDate = new Date()) => {
   };
 };
 
+/** Get the start of the day of the provided date.
+ * Notice: this will change the provided date.
+ * If you don't want this use it like: startOfDay(new Date(originalDate))
+ */
 export const startOfDay = (date = new Date()) => {
   date.setHours(0, 0, 0, 0);
   return date;
 };
 
+/** Get the end of the day of the provided date.
+ * Notice: this will change the provided date.
+ * If you don't want this use it like: startOfDay(new Date(originalDate))
+ */
 export const endOfDay = (date = new Date()) => {
   date.setHours(23, 59, 59, 999);
   return date;
