56450d4d4d8ce388100a16f8749af18e022e567b
simone <simonegauli@gmail.com>
2025-04-02 15:11:55 +0200

    ok

--- packages/node/src/log.ts
+++ packages/node/src/log.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { inspect } from 'node:util';
 import { temporaryFile } from './tempy.js';
 import fs from 'node:fs/promises';
@@ -6,21 +7,19 @@ import fs from 'node:fs/promises';
 export const logToFile = async (data: string, { extension = 'json', title = 'Log stored at:' } = {}) => {
   const file = await temporaryFile({ extension });
   await fs.writeFile(file, data);
-  // eslint-disable-next-line no-console
   console.log(title, file);
 };
 
-export const log = (message?: unknown, ...opts: unknown[]) => {
-  // eslint-disable-next-line no-console
-  console.log(inspectLog(message), opts.map((t) => inspectLog(t)).join(' '));
-};
-
-// eslint-disable-next-line sonarjs/function-return-type
-const inspectLog = (message: unknown) => {
-  if (typeof message === 'string' || typeof message === 'number') return message;
-  return inspect(message, {
-    maxArrayLength: Infinity,
-    depth: Infinity,
-    colors: true,
-  });
+export const inspectLog = (message: unknown) => {
+  if (typeof message === 'string' || typeof message === 'number') {
+    console.log(message);
+    return;
+  }
+  console.log(
+    inspect(message, {
+      maxArrayLength: Infinity,
+      depth: Infinity,
+      colors: true,
+    }),
+  );
 };
