64d96820c2263eb781c2df45ff63d97da50b1946
simone <simonegauli@gmail.com>
2025-04-05 17:06:54 +0200

    ok

--- packages/node/src/log.ts
+++ packages/node/src/log.ts
@@ -3,13 +3,6 @@ import { inspect } from 'node:util';
 import { temporaryFile } from './tempy.js';
 import fs from 'node:fs/promises';
 
-/** @deprecated use storage.logToFile, this is just for temporary usage. */
-export const logToFile = async (data: string, { extension = 'json', title = 'Log stored at:' } = {}) => {
-  const file = await temporaryFile({ extension });
-  await fs.writeFile(file, data);
-  console.log(title, file);
-};
-
 export const inspectLog = (message: unknown) => {
   if (typeof message === 'string' || typeof message === 'number') {
     console.log(message);
