775d4c4eec856c499c47f29af49187177febcf9d
simone <simonegauli@gmail.com>
2025-04-05 19:55:29 +0200

    fix unused

--- packages/node/src/log.ts
+++ packages/node/src/log.ts
@@ -1,7 +1,5 @@
 /* eslint-disable no-console */
 import { inspect } from 'node:util';
-import { temporaryFile } from './tempy.js';
-import fs from 'node:fs/promises';
 
 export const inspectLog = (message: unknown) => {
   if (typeof message === 'string' || typeof message === 'number') {
