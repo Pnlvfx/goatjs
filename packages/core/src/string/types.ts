d4cb98cad9f4e26c1ab3463776074b32d5330cb6
simone <simonegauli@gmail.com>
2025-06-08 01:44:14 +0200

    add new functions

---
+++ packages/core/src/string/types.ts
@@ -0,0 +1,18 @@
+/**
+ * Supported locale values. Use `false` to ignore locale.
+ * Defaults to `undefined`, which uses the host environment.
+ */
+export type Locale = string[] | string | false | undefined;
+
+/**
+ * Options used for converting strings to any case.
+ */
+export interface CaseOptions {
+  locale?: Locale;
+  split?: (value: string) => string[];
+  /** @deprecated Pass `split: splitSeparateNumbers` instead. */
+  separateNumbers?: boolean;
+  delimiter?: string;
+  prefixCharacters?: string;
+  suffixCharacters?: string;
+}
