8564e5157ea6e15c60d645f7c255264d7c4c5b63
simone <simonegauli@gmail.com>
2025-06-08 18:41:38 +0200

    typed-object => object

--- packages/core/src/object.ts
+++ packages/core/src/object.ts
@@ -8,15 +8,6 @@ export const getKeys = <T extends object>(obj: T) => {
   return Object.keys(obj) as (keyof T)[];
 };
 
-/** Use this instead of Object.keys to get readonly keys.
- * Please notice that this will give "fake" readonly keys
- * We are not really freezing the keys
- * If you want to do that use Object.freeze on your own before passing the object here.
- */
-export const getReadonlyKeys = <T extends object>(obj: T) => {
-  return Object.keys(obj) as unknown as readonly (keyof T)[];
-};
-
 /** Use this instead of Object.values to get typed values. */
 export const getValues = <T extends object>(obj: T) => {
   return Object.values(obj) as T[keyof T][];
