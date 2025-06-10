4531c32c6755edb5fff2c639b9bffebc93fd4498
Simone Gauli <simone97_19@hotmail.it>
2025-06-10 22:00:21 +0200

    TODO

--- packages/rimraf/src/rimraf.ts
+++ packages/rimraf/src/rimraf.ts
@@ -1,5 +1,7 @@
 import fs from 'node:fs/promises';
 
+// todo ADD A CHECK TO DON'T ALLOW DELETING FILES OUTSIDE THE PACKAGE.JSON
+
 export const rimraf = async (paths: string | string[]) => {
   const array = Array.isArray(paths) ? paths : [paths];
   for (const path of array) {
