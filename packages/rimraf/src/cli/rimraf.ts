b16322802be2248e64829d34f87c1c6f04350268
Simone Gauli <simone97_19@hotmail.it>
2025-06-10 16:06:20 +0200

    fix rimraf params

--- packages/rimraf/src/cli/rimraf.ts
+++ packages/rimraf/src/cli/rimraf.ts
@@ -5,7 +5,7 @@ import { rimraf } from '../rimraf.js';
 const run = async () => {
   try {
     const args = process.argv.slice(2);
-    await rimraf(...args);
+    await rimraf(args);
     process.exit(0);
   } catch (err) {
     // eslint-disable-next-line no-console
--- packages/rimraf/src/rimraf.ts
+++ packages/rimraf/src/rimraf.ts
@@ -1,7 +1,8 @@
 import fs from 'node:fs/promises';
 
-export const rimraf = async (...paths: string[]) => {
-  for (const path of paths) {
+export const rimraf = async (paths: string | string[]) => {
+  const array = Array.isArray(paths) ? paths : [paths];
+  for (const path of array) {
     await fs.rm(path, { recursive: true, force: true });
   }
 };
