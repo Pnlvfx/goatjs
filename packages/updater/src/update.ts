0a0555526c2b813fcb0a62251582d822c33c5d31
Simone Gauli <simone97_19@hotmail.it>
2025-06-13 11:15:21 +0200

    simplify updater

--- packages/updater/src/update.ts
+++ packages/updater/src/update.ts
@@ -1,37 +1,14 @@
 /* eslint-disable no-console */
-import { spawn } from 'node:child_process';
-import { platform } from 'node:os';
-
 // try to move into his own package so we can use it here too.
 
-const isWindows = platform() === 'win32';
+import { exec } from 'node:child_process';
+import { promisify } from 'node:util';
 
-export const updateLocalDeps = (packages: Record<string, string>) => {
-  return new Promise<void>((resolve, reject) => {
-    const deps = [];
-    for (const [key, value] of Object.entries(packages)) {
-      deps.push(`${key}@${value}`);
-    }
-    // eslint-disable-next-line sonarjs/no-os-command-from-path
-    const child = spawn('yarn', ['up', deps.join(' ')], { shell: isWindows });
+const execAsync = promisify(exec);
 
-    child.on('error', reject);
-
-    let error = '';
-
-    child.stderr.on('data', (chunk: Buffer) => {
-      const err = chunk.toString();
-      console.log(err);
-      error += err;
-    });
-
-    child.stdout.on('data', (chunk: Buffer) => {
-      console.log(chunk.toString());
-    });
-
-    child.on('close', (code) => {
-      if (code === 0) resolve();
-      else reject(new Error(`Update failed with code: ${code?.toString() ?? ''} because of: ${error}`));
-    });
-  });
+export const updateLocalDeps = async (packages: Record<string, string>) => {
+  const deps = Object.entries(packages).map(([name, version]) => `${name}@${version}`);
+  const { stderr, stdout } = await execAsync(`yarn up ${deps.join(' ')}`);
+  console.log(stderr);
+  console.log(stdout);
 };
