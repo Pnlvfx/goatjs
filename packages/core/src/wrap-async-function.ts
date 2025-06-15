d7343bc717b5419e0899ec31d79121b931628f8d
simone <simonegauli@gmail.com>
2025-06-15 19:12:35 +0200

    move wrap async function to node package

--- packages/core/src/wrap-async-function.ts
+++ packages/core/src/wrap-async-function.ts
@@ -1,4 +1,10 @@
-/** @deprecated This is not a good solution to your problem */
+/** This is not a good solution for client side applications,
+ * that why we moved it on the node package, if you are in server side you can use the
+ * node export as this will be removed soon.
+ * this can affect how event dom behave, you must wrap your function in your own,
+ * especially if you are using the event parameter.
+ * @deprecated
+ */
 export const wrapAsyncFunction = <ARGS extends unknown[]>(fn: (...args: ARGS) => Promise<unknown>): ((...args: ARGS) => void) => {
   return (...args) => {
     void fn(...args);
