fe069ccf6821a4d39366062eb7e0ead1dd703d83
Simone Gauli <simone97_19@hotmail.it>
2025-06-23 14:34:57 +0200

    improve updater

--- packages/updater/src/update.ts
+++ packages/updater/src/update.ts
@@ -1,16 +1,26 @@
 /* eslint-disable no-console */
-// try to move into his own package so we can use it here too.
+// TODO try to move into his own package so we can use it here too.
 
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
 
+interface Options {
+  readonly debug?: boolean;
+}
+
 const execAsync = promisify(exec);
 
-export const updateLocalDeps = async (packages: Record<string, string>) => {
+export const updateLocalDeps = async (packages: Record<string, string>, { debug }: Options = {}) => {
   const deps = Object.entries(packages).map(([name, version]) => `${name}@${version}`);
   const command = `yarn up ${deps.join(' ')}`;
-  console.log(`running ${command}`);
+  if (debug) {
+    console.log(`running ${command}`);
+  }
   const { stderr, stdout } = await execAsync(command);
-  console.log(stderr);
-  console.log(stdout);
+  if (stderr) {
+    console.log(stderr);
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
 };
