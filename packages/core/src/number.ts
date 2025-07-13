775a7c357f32c912e828ef9fadb147c43e08ba71
Simone Gauli <simone97_19@hotmail.it>
2025-07-13 03:47:26 +0200

    allow literal recognizion for toNumber

--- packages/core/src/number.ts
+++ packages/core/src/number.ts
@@ -1,7 +1,9 @@
-export const toNumber = (string: string) => {
+type StringToNumber<T extends string> = T extends `${infer N extends number}` ? N : number;
+
+export const toNumber = <T extends string>(string: T) => {
   const num = Number(string);
   if (Number.isNaN(num)) throw new Error(`"${string}" is not a valid number.`);
-  return num;
+  return num as StringToNumber<T>;
 };
 
 export const numberToString = <K extends number>(number: K) => {
