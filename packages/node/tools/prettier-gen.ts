bf96b6bfb9ae05b6b6f836a6b8471a0b7d9fdc4d
simone <simonegauli@gmail.com>
2025-07-19 12:09:38 +0200

    add tsconfig for nextjs projects

--- packages/node/tools/prettier-gen.ts
+++ packages/node/tools/prettier-gen.ts
@@ -1,12 +1,11 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { pathExist } from '../src/fs.js';
 
 const compiledPath = path.join('src', 'compiled');
 
-if (!(await pathExist(compiledPath))) {
+try {
   await fs.mkdir(compiledPath, { recursive: true });
-}
+} catch {}
 const rootPrettier = path.join('..', '..', '.prettierrc');
 const prettierCompiled = path.join(compiledPath, 'prettier-configs.json');
 await fs.cp(rootPrettier, prettierCompiled);
