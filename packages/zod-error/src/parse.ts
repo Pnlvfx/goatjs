59cb28ea517ab4c5d9708f7b37832e6b733db709
simone <simonegauli@gmail.com>
2025-08-02 03:51:52 +0200

    fix zod error data

--- packages/zod-error/src/parse.ts
+++ packages/zod-error/src/parse.ts
@@ -3,6 +3,6 @@ import { validationError } from './error.js';
 
 export const parseAsync = async <T extends z.$ZodType>(schema: T, value: unknown, _ctx?: z.ParseContext<z.$ZodIssue>): Promise<z.output<T>> => {
   const { success, error, data } = await z.safeParseAsync(schema, value, _ctx);
-  if (!success) throw validationError(error.message, { data, zodError: error });
+  if (!success) throw validationError(error.message, { data: value });
   return data;
 };
