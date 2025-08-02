ac72cda14d17d2b49b27b3242364160d6f8c9d06
simone <simonegauli@gmail.com>
2025-08-02 03:43:18 +0200

    drop zodError, use message instead

--- packages/zod-error/src/error.ts
+++ packages/zod-error/src/error.ts
@@ -1,19 +1,15 @@
-import type * as z from 'zod/v4/core';
-
 class ValidationError extends Error {
   data: unknown;
-  zodError: z.$ZodError;
-  constructor(message: string, data: unknown, zodError: z.$ZodError) {
+  constructor(message: string, data: unknown) {
     super(message);
     this.name = 'ValidationError';
     this.data = data;
-    this.zodError = zodError;
     Object.setPrototypeOf(this, ValidationError.prototype);
   }
 }
 
-export const validationError = (message: string, { data, zodError }: { data: unknown; zodError: z.$ZodError }) => {
-  return new ValidationError(message, data, zodError);
+export const validationError = (message: string, { data }: { data: unknown }) => {
+  return new ValidationError(message, data);
 };
 
 export const isValidationError = (err: unknown) => {
