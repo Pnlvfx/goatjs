a8853c2202646016d8e3e3817383dc38b614af23
simone <simonegauli@gmail.com>
2025-08-05 17:04:29 +0200

    some package refractor

--- packages/dbz/src/dbz/index.ts
+++ packages/dbz/src/dbz/index.ts
@@ -24,11 +24,12 @@ export const dbz = {
     await (platform() === 'win32' ? execAsync(`set YARN_NPM_AUTH_TOKEN=${token}`) : execAsync(`export YARN_NPM_AUTH_TOKEN="${token}"`));
   },
   publish: async ({ version }: DbzPublishOptions = {}) => {
+    await checkGitStatus();
     const monorepo = await isMonorepo();
     if (monorepo) {
       consoleColor('yellow', "dbz detect that you're running in a monorepo. Please ensure to run this scripts from the root.");
+      await spawnStdio('yarn', ['build']);
     }
-    await checkGitStatus();
     await publish({ version, monorepo });
     await git.add();
     await git.commit('RELEASE');
