d0b2f67a0ad37a5d72fd0a73808503232db6b0ca
simone <simonegauli@gmail.com>
2025-08-05 19:51:22 +0200

    drop cookie helper

--- packages/node/src/cookies.ts
+++
@@ -1,5 +0,0 @@
-export const getCookieDomain = (url: string) => {
-  const domain = new URL(url).hostname.split('.').slice(-2).join('.');
-  if (domain === 'localhost') return domain;
-  return `.${domain}`;
-};
