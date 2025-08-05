d955a906176a643b1ec7c05ed63b738f7271fd13
simone <simonegauli@gmail.com>
2025-08-05 23:36:43 +0200

    fix stdio error handling

--- packages/node/src/terminal/stdio.ts
+++ packages/node/src/terminal/stdio.ts
@@ -1,9 +1,12 @@
 import { spawn } from 'node:child_process';
 
 export const spawnStdio = (command: string, args: string[]) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve, reject) => {
     const child = spawn(command, args, { stdio: 'inherit' });
     child.on('error', reject);
-    child.on('close', resolve);
+    child.on('close', (code) => {
+      if (code === 0) resolve();
+      else reject(new Error('Spawned process failed'));
+    });
   });
 };
