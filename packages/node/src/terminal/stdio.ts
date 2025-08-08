bbb7b9e057a9f66d8ebd5e8c0913291c8dbace7c
simone <simonegauli@gmail.com>
2025-08-08 21:01:09 +0200

    fix spawn on windows (maybe)

--- packages/node/src/terminal/stdio.ts
+++ packages/node/src/terminal/stdio.ts
@@ -1,6 +1,10 @@
 import { spawn } from 'node:child_process';
+import os from 'node:os';
+
+const platform = os.platform();
 
 export const spawnStdio = (command: string, args: string[]) => {
+  command = platform === 'win32' && !command.endsWith('.cmd') ? `${command}.cmd` : command;
   return new Promise<void>((resolve, reject) => {
     const child = spawn(command, args, { stdio: 'inherit' });
     child.on('error', reject);
