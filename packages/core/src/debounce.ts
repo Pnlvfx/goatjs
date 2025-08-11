1bce7e0a9f52b680a0a36b2048f535081cfeefe4
simone <simonegauli@gmail.com>
2025-08-11 15:11:01 +0200

    add debounce

---
+++ packages/core/src/debounce.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export const debounce = <T extends Function, Args extends (...args: unknown[]) => T>(func: T, wait: number, leading = false) => {
+  let timeout: NodeJS.Timeout | undefined;
+
+  return (...args: Parameters<Args>) => {
+    clearTimeout(timeout);
+    if (leading && !timeout) {
+      func.apply(this, args);
+    }
+    timeout = setTimeout(() => {
+      timeout = undefined;
+      if (!leading) {
+        func.apply(this, args);
+      }
+    }, wait);
+  };
+};
