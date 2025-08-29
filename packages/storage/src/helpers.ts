6082b57b5a8b4f8d4d6af6aae3934ea90985f652
simone <simonegauli@gmail.com>
2025-08-29 22:57:14 +0200

    fix project name

--- packages/storage/src/helpers.ts
+++ packages/storage/src/helpers.ts
@@ -3,8 +3,11 @@ import fs from 'node:fs/promises';
 
 export const getProjectName = async () => {
   const { name } = await getRootPkgJSON();
+  /** if name is @goatjs/core scope is @goatjs and scopeName is core
+   * but if name is core scope is core and scopeName is undefined
+   */
   const [scope, scopeName] = name.split('/');
-  return { scope: scope?.slice(1), name: scopeName ?? name.replace('api-', '') };
+  return { scope: scope?.replace('@', ''), name: scopeName ?? name.replace('api-', '') };
 };
 
 export const mkDir = async (folder: string, recursive?: boolean) => {
