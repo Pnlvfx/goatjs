28ec3764d55edf49df21e4eae4f57f1d2ff1e4cf
simone <simonegauli@gmail.com>
2025-08-30 11:15:13 +0200

    add faster capitalize way

---
+++ packages/core/src/capitalize.ts
@@ -0,0 +1,5 @@
+export const  capitalize = (string: string) => {
+    const firstLetter = string.at(0);
+    if (!firstLetter) return string;
+    return firstLetter.toUpperCase() + string.slice(1);
+}
