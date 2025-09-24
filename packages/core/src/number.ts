3da9ba7e3eb3f4228bfaa3d626dcf190ced5d7c9
Simone Gauli <simone97_19@hotmail.it>
2025-09-24 22:27:02 +0200

    fix empty string wrongly converted to number

--- packages/core/src/number.ts
+++ packages/core/src/number.ts
@@ -1,6 +1,7 @@
 type StringToNumber<T extends string> = T extends `${infer N extends number}` ? N : number;
 
 export const toNumber = <T extends string>(string: T) => {
+  if (string.trim() === '') throw new Error(`"${string}" is not a valid number.`);
   const num = Number(string);
   if (Number.isNaN(num)) throw new Error(`"${string}" is not a valid number.`);
   return num as StringToNumber<T>;
