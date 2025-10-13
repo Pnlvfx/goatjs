c2b41246989d1e3c7a16bedd875598cb87b547fc
Simone Gauli <simonegauli@gmail.com>
2025-10-13 22:11:57 +0200

    bump deps

--- packages/dbz/eslint.config.js
+++ packages/dbz/eslint.config.js
@@ -1,4 +1,4 @@
 import { nodeMonorepoConfigs } from '@goatjs/node-monorepo-eslint';
 import { defineConfig } from '@eslint/config-helpers';
 
-export default defineConfig(nodeMonorepoConfigs({ ignores: ['dist'], tsconfigRootDir: import.meta.dirname }));
+export default defineConfig([{ ignores: ['dist'] }, ...nodeMonorepoConfigs({ tsconfigRootDir: import.meta.dirname })]);
