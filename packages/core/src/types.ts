c55ccd73012a64300d7f5f43140b0396c8ff436b
Simone Gauli <simonegauli@gmail.com>
2025-10-17 19:21:06 +0200

    add flat object keys util

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,2 +1,8 @@
 export type NonPromiseCallback<T, Args extends unknown[] = []> = (...args: Args) => T;
 export type Callback<T, Args extends unknown[] = []> = (...args: Args) => Promise<T> | T;
+
+export type FlatObjectKeys<T extends Record<string, unknown>, Key = keyof T> = Key extends string
+  ? T[Key] extends Record<string, unknown>
+    ? `${Key}.${FlatObjectKeys<T[Key]>}`
+    : `${Key}`
+  : never;
