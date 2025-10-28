8ff3bf941cfd2fb25606d2e3a3e9dee9e275ab55
Simone Gauli <simonegauli@gmail.com>
2025-10-28 18:47:20 +0100

    fixed options for object

--- packages/core/src/object/camel.ts
+++ packages/core/src/object/camel.ts
@@ -1,4 +1,3 @@
-import type { PascalCaseOptions } from '../string/options.js';
 import { camelCase, type CamelCase } from '../string/camel.js';
 
 export type Camelize<T> = T extends (infer U)[]
@@ -9,13 +8,15 @@ export type Camelize<T> = T extends (infer U)[]
       }
     : T;
 
-export const camelizeObject = <T extends object>(obj: T, options?: PascalCaseOptions) => {
+const objectOptions = { mergeAmbiguousCharacters: true };
+
+export const camelizeObject = <T extends object>(obj: T) => {
   const response: Record<string, unknown> = {};
   for (const [key, value] of Object.entries(obj)) {
     if (typeof value === 'object') {
-      response[camelCase(key, options)] = camelizeObject(value, options);
+      response[camelCase(key, objectOptions)] = camelizeObject(value);
     } else {
-      response[camelCase(key, options)] = value;
+      response[camelCase(key, objectOptions)] = value;
     }
   }
 
