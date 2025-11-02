8ca1157b7d7eb69a35388f2a017473fbcd25e92c
Simone Gauli <simonegauli@gmail.com>
2025-11-02 20:35:56 +0100

    add throttle

---
+++ packages/core/src/throttle.ts
@@ -0,0 +1,13 @@
+export const throttle = <T extends (...args: never[]) => unknown>(func: T, limit: number): ((...args: Parameters<T>) => void) => {
+  let inThrottle = false;
+
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => {
+        inThrottle = false;
+      }, limit);
+    }
+  };
+};
