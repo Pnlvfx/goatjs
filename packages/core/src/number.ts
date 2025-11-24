570062caec979f5d572e4267fdf5901e1c1d1107
Simone Gauli <simonegauli@gmail.com>
2025-11-24 20:47:05 +0100

    add new isNumber function to help circumvent the issue where we always check for

--- packages/core/src/number.ts
+++ packages/core/src/number.ts
@@ -1,3 +1,19 @@
+export const numberToString = <K extends number>(number: K) => number.toString() as `${K}`;
+
+// most of the time we do if (!) on a number
+// which will also affect 0 and maybe other numbers,
+// this will correctly check
+export const isNumber = (value: string | number | undefined | null) => {
+  if (typeof value === 'number') return true;
+  if (value === undefined || value === null) return false;
+  try {
+    toNumber(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 type StringToNumber<T extends string> = T extends `${infer N extends number}` ? N : number;
 
 export const toNumber = <T extends string>(string: T) => {
@@ -5,8 +21,4 @@ export const toNumber = <T extends string>(string: T) => {
   const num = Number(string);
   if (Number.isNaN(num)) throw new Error(`"${string}" is not a valid number.`);
   return num as StringToNumber<T>;
-};
-
-export const numberToString = <K extends number>(number: K) => {
-  return number.toString() as `${K}`;
 };
--- packages/node/src/sanitize.ts
+++ packages/node/src/sanitize.ts
@@ -1,6 +1,6 @@
 import nativeSanitize from 'sanitize-filename';
 
-// TODO [2025-12-30] use this and remove sanitize-filename (note that actually it doesn't work!)
+// TODO [2026-12-30] use this and remove sanitize-filename (note that actually it doesn't work!)
 
 // const illegalRe = /["*/:<>?\\|]/g;
 // const controlRe = /[\u0000-\u001F\u0080-\u009F]/g;
