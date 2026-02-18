4f6ebfe2f8ef5ad10907ee377a18642c788c3cca
Simone Gauli <simonegauli@gmail.com>
2026-02-18 15:42:46 +0100

    BREAKING CHANGE ON STORE

--- packages/storage/src/store.ts
+++ packages/storage/src/store.ts
@@ -1,23 +1,19 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import type * as z from 'zod';
+import { storage } from './storage.ts';
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { storage } from './storage.ts';
 
-/** This mimic the browser localStorage and allow you to store
- *  primitives on disk.
- */
-export const createStore = async <T extends z.ZodType>(name: string, schema: T, { root: baseRoot }: { root?: string } = {}) => {
-  const base = baseRoot ?? (await storage.use('store'));
+export interface StoreParams<T extends z.ZodType> {
+  root?: string;
+  initial?: z.infer<T>;
+}
+
+/** This mimic the browser localStorage and allow you to store primitives on disk. */
+export const createStore = async <T extends z.ZodType>(name: string, schema: T, { root: baseRoot, initial }: StoreParams<T> = {}) => {
   type StoreType = z.infer<T>;
-  const root = path.join(base, name);
-
-  try {
-    await fs.mkdir(root);
-  } catch {}
-
-  const configFile = path.join(root, 'configs.json');
-
+  const base = baseRoot ?? (await storage.use('store'));
+  const configFile = path.join(base, `${name}.json`);
   let currentConfig: StoreType | undefined;
 
   const getBuffer = async () => {
@@ -29,21 +25,32 @@ export const createStore = async <T extends z.ZodType>(name: string, schema: T, 
     }
   };
 
-  return {
-    get: async () => {
-      if (!currentConfig) {
-        const buf = await getBuffer();
-        if (!buf) return;
-        currentConfig = JSON.parse(buf.toString()) as StoreType;
-      }
+  const get = async () => {
+    if (!currentConfig) {
+      const buf = await getBuffer();
+      if (!buf) return;
+      currentConfig = JSON.parse(buf.toString()) as StoreType;
+    }
 
-      return currentConfig;
-    },
-    set: async (configs: Partial<StoreType>) => {
-      const update = currentConfig ? { ...currentConfig, ...configs } : configs;
-      currentConfig = await schema.parseAsync(update);
-      await fs.writeFile(configFile, JSON.stringify(currentConfig));
-    },
+    return currentConfig;
+  };
+
+  const set = async (configs: Partial<StoreType>) => {
+    const update = currentConfig ? { ...currentConfig, ...configs } : configs;
+    currentConfig = await schema.parseAsync(update);
+    await fs.writeFile(configFile, JSON.stringify(currentConfig));
+  };
+
+  if (initial) {
+    const stored = await get();
+    if (!stored) {
+      await set(initial);
+    }
+  }
+
+  return {
+    get,
+    set,
     clear: async () => {
       try {
         await fs.rm(configFile);
