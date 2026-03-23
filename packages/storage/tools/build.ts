36d763893679fd48a22b7907965e560661546d80
Simone Gauli <simonegauli@gmail.com>
2026-03-23 23:03:29 +0100

    fuck em

--- packages/storage/tools/build.ts
+++ packages/storage/tools/build.ts
@@ -1,6 +1,6 @@
 import { rimraf } from '@goatjs/rimraf';
 import { spawnWithLog } from '@goatjs/dbz/spawn';
 
-await rimraf(['dist', 'tsconfig.build.tsbuildinfo']);
+await rimraf('dist');
 await spawnWithLog('eslint');
 await spawnWithLog('tsc', ['-p', 'tsconfig.build.json']);
