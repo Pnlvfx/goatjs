927828f79032bef755b50f01d603588f8ef2c7c5
Simone Gauli <simonegauli@gmail.com>
2026-03-23 22:48:13 +0100

    mah

--- packages/link-preview/tools/build.ts
+++ packages/link-preview/tools/build.ts
@@ -1,6 +1,6 @@
 import { rimraf } from '@goatjs/rimraf';
 import { spawnWithLog } from '@goatjs/dbz/spawn';
 
-await rimraf('dist');
+await rimraf(['dist', 'tsconfig.build.tsbuildinfo']);
 await spawnWithLog('eslint');
 await spawnWithLog('tsc', ['-p', 'tsconfig.build.json']);
--- packages/rimraf/tools/build.ts
+++ packages/rimraf/tools/build.ts
@@ -1,6 +1,6 @@
 import { spawnWithLog } from '@goatjs/dbz/spawn';
 import { rimraf } from '../src/rimraf.ts';
 
-await rimraf('dist');
+await rimraf(['dist', 'tsconfig.build.tsbuildinfo']);
 await spawnWithLog('eslint');
 await spawnWithLog('tsc', ['-p', 'tsconfig.build.json']);
