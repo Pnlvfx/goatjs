12b728986b3783cd5ed219ac36ab8737ea23c572
Simone Gauli <simonegauli@gmail.com>
2026-04-26 14:14:03 +0200

    vps

--- packages/vps/src/internal-plugins/certbot.ts
+++ packages/vps/src/internal-plugins/certbot.ts
@@ -2,5 +2,5 @@ import type { PluginContext } from '../types/plugin.ts';
 
 export const certbot = async ({ ssh, nginx: { serverName } }: PluginContext) => {
   await ssh.execCommand('sudo snap install certbot --classic');
-  await ssh.execCommand(`sudo certbot --nginx -d ${serverName}`);
+  await ssh.execCommand(`sudo certbot --nginx -d ${serverName}`, { stdin: 'inherit' });
 };
