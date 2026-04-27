cd554ffee878f8a2fd6229852fa0852d9a598010
Simone Gauli <simonegauli@gmail.com>
2026-04-27 17:18:16 +0200

    fix core error parser

--- packages/core/src/env.ts
+++ packages/core/src/env.ts
@@ -1,4 +1,4 @@
-export const getSanitizedConfig = <T extends object>(config: Partial<T>) => {
+export const getSanitizedConfig = <T extends Record<string, string>>(config: Partial<T>) => {
   for (const [key, value] of Object.entries(config)) {
     if (value === undefined) throw new Error(`Missing key ${key}`);
   }
--- packages/core/src/error.ts
+++ packages/core/src/error.ts
@@ -1,13 +1,17 @@
 import { isFetchError } from './errors/fetch.ts';
 
 export const parseError = (err: unknown) => {
-  if (Error.isError(err) || isFetchError(err)) return err;
+  // error.isError is suggested and should replace instanceof
+  // but for this helper we still need to parse whatever we get so we used both
+  if (Error.isError(err) || err instanceof Error || isFetchError(err)) return err;
   return new Error(errorToString(err));
 };
 
 const errorToString = (err: unknown) => {
   let error = '';
-  if (typeof err === 'string') {
+  if (typeof err === 'object' && err !== null && 'message' in err && err.message === 'string') {
+    error = err.message;
+  } else if (typeof err === 'string') {
     error += err;
   } else {
     try {
