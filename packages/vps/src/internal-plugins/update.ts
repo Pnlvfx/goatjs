9adc2adce1f9a3bd7837b5986139feece6b7553f
Simone Gauli <simonegauli@gmail.com>
2026-05-10 17:54:15 +0200

    imp

--- packages/vps/src/internal-plugins/update.ts
+++ packages/vps/src/internal-plugins/update.ts
@@ -3,4 +3,5 @@ import type { Ssh } from '../ssh.ts';
 export const updateSystem = async (ssh: Ssh) => {
   await ssh.execCommand('sudo apt update');
   await ssh.execCommand('sudo apt upgrade -y');
+  await ssh.execCommand('sudo apt autoremove');
 };
